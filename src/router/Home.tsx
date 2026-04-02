84be54c7ce29ab012460c8e371713444d7ec7f70
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-01 23:49:19 -0300

    Exibindo node do usuário

--- src/router/Home.tsx
+++ src/router/Home.tsx
@@ -1,7 +1,7 @@
 import "./Home.css";
 import Search from "../components/Search";
 import { useState } from "react";
-import type { UserProps } from "../components/types/user";
+import type { UserProps } from "../types/user";
 
 const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
@@ -11,12 +11,23 @@ const Home = () => {
 
     const data = await res.json();
 
-    console.log(data);
+    const { avatar_url, login, location, followers, following } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers,
+      following,
+    };
+
+    setUser(userData);
   };
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}
     </div>
   );
 };
