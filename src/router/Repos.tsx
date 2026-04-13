0f4debd3b976337ecb6f5e3e99d74c25856d5bf5
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-13 11:56:05 -0300

    Arrumando erro

--- src/router/Repos.tsx
+++ src/router/Repos.tsx
@@ -3,10 +3,33 @@ import classes from "./Repos.module.css";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+import type { RepoProps } from "../types/repo";
+
 import BackBtn from "../components/BackBtn";
+import Loader from "../components/Loader";
 
 const Repos = () => {
   const { username } = useParams();
+
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const loadRepos = async function (username: string) {
+      const res = await fetch(`https://api.github.com/users/${username}/repos`);
+
+      const data = await res.json();
+
+      setIsLoading(false);
+
+      console.log(data);
+    };
+
+    loadRepos(username);
+  }, []);
 
   return (
     <div>
