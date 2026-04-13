9b1a8d014e4d21d850622fa7975a9409a47e0d09
Duda Rebouças <mariadudareboucas@gmail.com>
2026-04-13 15:14:06 -0300

    Componente de repositório

--- src/router/Repos.tsx
+++ src/router/Repos.tsx
@@ -7,6 +7,7 @@ import type { RepoProps } from "../types/repo";
 
 import BackBtn from "../components/BackBtn";
 import Loader from "../components/Loader";
+import Repo from "../components/Repo";
 
 const Repos = () => {
   const { username } = useParams();
@@ -43,7 +44,7 @@ const Repos = () => {
       {repos && repos.length > 0 && (
         <div>
           {repos?.map((repo: RepoProps) => (
-            <p>{repo.name}</p>
+            <Repo key={repo.name} {...repo} />
           ))}
         </div>
       )}
